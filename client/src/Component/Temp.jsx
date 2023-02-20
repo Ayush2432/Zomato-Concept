8f8a5dc5a9914ed16c8bc2dc20bd6c233fdf22f1
Ayush2432 <ayush2432pandey@gmail.com>
2023-02-21 00:19:48 +0530

    FrontEnd

---
+++ client/src/Component/Temp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Temp = () => {
+    return (
+        <div className="container px-4 mx-auto">
+            Hello this is a temp component
+        </div>
+    )
+}
+
+export default Temp;
